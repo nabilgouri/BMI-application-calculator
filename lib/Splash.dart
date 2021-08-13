27b9a25e7b27313dacc6471aeb4d6c15b3be6b96
nabilgouri <38146132+nabilgouri@users.noreply.github.com>
2021-08-13 02:04:13 +0100

    Added a simple splash screen

---
+++ lib/Splash.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+import 'inputPage.dart';
+
+class Splash_screen extends StatefulWidget {
+  @override
+  _Splash_screenState createState() => _Splash_screenState();
+}
+
+class _Splash_screenState extends State<Splash_screen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  _navigate() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => InputPage()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF111328),
+        child: Center(
+          child: Text(
+            'BMI APP',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
